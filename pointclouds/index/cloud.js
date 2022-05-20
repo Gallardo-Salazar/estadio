ab0a225a982123067fdcb9de981c4e041e198131
unknown <gallardoirg@gmail.com>
2022-05-20 18:42:24 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 29955687,
+    "boundingBox": {
+        "lx": 659873.9579925537,
+        "ly": 2287368.8118896486,
+        "lz": 1645.377399673462,
+        "ux": 660474.3094024658,
+        "uy": 2287969.1632995607,
+        "uz": 2245.728809585571
+    },
+    "tightBoundingBox": {
+        "lx": 659873.958,
+        "ly": 2287368.8119,
+        "lz": 1645.3774,
+        "ux": 660367.4779,
+        "uy": 2287969.1633,
+        "uz": 1704.7992
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 5.199195861816406,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
